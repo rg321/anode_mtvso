546a9ddc7291a922cf2d3fdbd1c8f6e47ed7d00a
raghavG <guptaraghu321@gmail.com>
2020-07-22 01:01:02 +0530

    changes for vso dataset

--- train.py
+++ train.py
@@ -33,6 +33,8 @@ import math
 import sys
 import os
 
+from data_helper import get_galaxyZoo_loaders
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--network', type = str, choices = ['resnet', 'sqnxt'], default = 'sqnxt')
@@ -108,12 +110,12 @@ transform_test  = transforms.Compose([
     transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
 ])
 
-train_dataset = torchvision.datasets.CIFAR10(root='../data', transform = transform_train, train = True, download = True)
-test_dataset = torchvision.datasets.CIFAR10(root='../data', transform = transform_test, train = False, download = True)
-train_loader = torch.utils.data.DataLoader(train_dataset, batch_size = batch_size, num_workers = 4, shuffle = True)
-test_loader = torch.utils.data.DataLoader(test_dataset, batch_size = 128, num_workers = 4, shuffle = False)
+# train_dataset = torchvision.datasets.CIFAR10(root='../data', transform = transform_train, train = True, download = True)
+# test_dataset = torchvision.datasets.CIFAR10(root='../data', transform = transform_test, train = False, download = True)
+# train_loader = torch.utils.data.DataLoader(train_dataset, batch_size = batch_size, num_workers = 4, shuffle = True)
+# test_loader = torch.utils.data.DataLoader(test_dataset, batch_size = 128, num_workers = 4, shuffle = False)
 
-# train_loader, test_loader, train_dataset = get_galaxyZoo_loaders(batch_size=args.batch_size, test_batch_size=args.test_batch_size)
+train_loader, test_loader, train_dataset = get_galaxyZoo_loaders(batch_size=args.batch_size, test_batch_size=args.test_batch_size)
 
 if args.dataset == 'MTVSO':
     num_classes = 20
